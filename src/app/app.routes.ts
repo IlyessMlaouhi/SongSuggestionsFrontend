9b20ffef0fb431bb008d0eacadf82f6e2b7d6529
ilyess <mlaouhiilyess@gmail.com>
2026-02-16 15:28:55 +0100

    updated the UI and added some functional changes for later fetching data with some mocked data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import {Profile} from './pages/profile/profile';
+import {Home} from './pages/home/home';
+import {Friends} from './pages/friends/friends';
+import {Signin} from './pages/signin/signin';
+import {Signup} from './pages/signup/signup';
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'profile', component: Profile },
+  { path: 'friends', component: Friends },
+  { path: 'signin', component: Signin },
+  { path: 'signup', component: Signup },
+  { path: '**', redirectTo: '' }
+];
---
+++ src/app/pages/signin/signin.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+  selector: 'app-signin',
+  imports: [RouterLink],
+  standalone: true,
+  templateUrl: './signin.html',
+  styleUrl: './signin.css',
+})
+export class Signin {
+
+}
